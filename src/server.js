fc5dcd427ac71ee89ccced9598ebd442bba51b7b
soonki-98 <alstnsrl98@gmail.com>
2022-08-08 23:18:52 +0900

    socket.io 설치

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import http from "http";
+import SocketIO from "socket.io";
 import express from "express";
-import WebSocket from "ws";
 
 const app = express();
 
@@ -9,27 +9,11 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 
+const httpServer = http.createServer(app);
+const io = SocketIO(httpServer);
+
 const handleListen = () => {
   console.log("Listening on http://localhost:3000");
 };
 
-const sockets = [];
-
-const handleConnection = (socket) => {
-  console.log("Connect to Browser");
-  sockets.push(socket);
-  socket.on("close", () => console.log("Disconnected from Browser"));
-  socket.on("message", (data, isBinary) => {
-    const { message, sender, id } = JSON.parse(data.toString());
-    sockets.forEach((s) => {
-      s.send(JSON.stringify({ msg: message, sender: sender || "익명", id }));
-    });
-  });
-};
-
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
-
-wss.on("connection", handleConnection);
-
-server.listen(3000, handleListen);
+httpServer.listen(3000, handleListen);
